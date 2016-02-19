4443dc27d691dc232932a79ababe251448983c32
Ben Gourley <bn@grly.me>
2016-02-19 18:10:23 +0000

    Add basic auth to homepage

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
 var express = require('express')
   , server = express()
+  , auth = require('basic-auth')
   , homepageView = require('./views/homepage')
 
 server.use(express.static(__dirname + '/static'))
 
 server.get('/', function (req, res, next) {
+
+  var credentials = auth(req)
+
+  if (!credentials || credentials.name !== 'guybrush' || credentials.pass !== 'mightypirate') {
+    res.statusCode = 401
+    res.setHeader('WWW-Authenticate', 'Basic realm="example"')
+    return res.end('Access denied')
+  }
+
   homepageView(req, function (err, html) {
     if (err) return next(err)
     res.send(html)
